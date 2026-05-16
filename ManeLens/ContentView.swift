3e9a5546bd1c17c40098bbc28b578bb9c3d4adf1
Shubham Raj <shubhamraj2202@gmail.com>
2026-05-16 14:17:15 +0900

    Initial Commit

---
+++ ManeLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ManeLens
+//
+//  Created by Shubham Raj on 16/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ManeLens/ManeLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManeLensApp.swift
+//  ManeLens
+//
+//  Created by Shubham Raj on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct ManeLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
